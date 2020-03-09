d948df430d4fccb571719445496d046c83c17137
jonacappelle <44842245+jonacappelle@users.noreply.github.com>
2020-03-09 11:41:33 +0100

    Update invert (before lab)

--- test/src/template.cu
+++ test/src/template.cu
@@ -1,19 +1,6 @@
-////////////////////////////////////////////////////////////////////////////
-//
-// Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
-//
-// Please refer to the NVIDIA end user license agreement (EULA) associated
-// with this source code for terms and conditions that govern your use of
-// this software. Any use, reproduction, disclosure, or distribution of
-// this software and related documentation outside the terms of the EULA
-// is strictly prohibited.
-//
-////////////////////////////////////////////////////////////////////////////
-
-/* Template project which demonstrates the basics on how to setup a project
-* example application.
-* Host code.
-*/
+////////////////////////////////////////////////////////////////////////////////
+// ADD / INVERT -- Jona Cappelle -- Jonas Bolle
+////////////////////////////////////////////////////////////////////////////////
 
 // includes, system
 #include <stdlib.h>
@@ -28,38 +15,62 @@
 #include <helper_cuda.h>
 #include <helper_functions.h> // helper functions for SDK examples
 
-//eigen includes
+// eigen includes
 #include "iostream"
 #include "cstdlib"
 
+extern "C"
+#define ARRAYSIZE 100 // Is also the number of threads that will be used
+#define BLOCKSIZE
 
 ////////////////////////////////////////////////////////////////////////////////
-// declaration, forward
-extern "C"
-void computeGold(float *reference, float *idata, const unsigned int len);
-
-
-// KERNEL
-
-
-
-
-
-#define ARRAYSIZE 100
-//#define BLOCKSIZE
+// KERNEL ADD
+////////////////////////////////////////////////////////////////////////////////
 int BLOCKSIZE;
-
 int nBlocks;
-
-
-// Kernel
-__global__ void add(int *a, int *b, int *out){
-	int idx = blockIdx.x*blockDim.x + threadIdx.x;
-	if (idx < ARRAYSIZE){
+// GPU
+__global__ void add(int *a, int *b, int *out)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < ARRAYSIZE)
+	{
 		out[idx] = a[idx] + b[idx];
 	}
 }
 
+// CPU
+void add(int *a, int *b, int *out)
+{
+	for (int i = 0; i < ARRAYSIZE; i++)
+	{
+		out[i] = a[i] + b[i];
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// KERNEL INVERT
+////////////////////////////////////////////////////////////////////////////////
+int BLOCKSIZE;
+int nBlocks;
+
+// GPU
+__global__ void invert(int *a, int *out)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < ARRAYSIZE)
+	{
+		out[idx] = a[ARRAYSIZE - idx]
+	}
+}
+
+// CPU
+void invert(int *a, int *out)
+{
+	for (int i = 0; i < ARRAYSIZE; i++)
+	{
+		out[i] = a[ARRAYSIZE - i];
+	}
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Program main
@@ -68,70 +79,78 @@ int main()
 {
 
 	//declare variables
-		int *a_host, *b_host, *out_host;
-		int *a_dev, *b_dev, *out_dev;
+	int *a_host, *b_host, *out_host;
+	int *a_dev, *b_dev, *out_dev;
 
+	//allocate arrays on host
+	a_host = (int *)malloc(ARRAYSIZE * sizeof(int));
 
-		//allocate arrays on host
-		a_host = (int*)malloc(ARRAYSIZE * sizeof(int));
+	b_host = (int *)malloc(ARRAYSIZE * sizeof(int));
+	out_host = (int *)malloc(ARRAYSIZE * sizeof(int));
 
-		b_host = (int *)malloc(ARRAYSIZE * sizeof(int));
-		out_host = (int *)malloc(ARRAYSIZE * sizeof(int));
-
-		//allocate arrays on device
-		cudaMalloc((void **) &a_dev, ARRAYSIZE * sizeof(int));
-		cudaMalloc((void **) &b_dev, ARRAYSIZE * sizeof(int));
-		cudaMalloc((void **) &out_dev, ARRAYSIZE * sizeof(int));
-
-
-	//rest of program (Other 4 steps go here)
+	//allocate arrays on device
+	cudaMalloc((void **)&a_dev, ARRAYSIZE * sizeof(int));
+	cudaMalloc((void **)&b_dev, ARRAYSIZE * sizeof(int));
+	cudaMalloc((void **)&out_dev, ARRAYSIZE * sizeof(int));
 
 	//Step 1: Copy data to GPU memory
-
 	cudaMemcpy(a_dev, a_host, ARRAYSIZE * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(b_dev, b_host, ARRAYSIZE * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(out_dev, out_host, ARRAYSIZE * sizeof(int), cudaMemcpyHostToDevice);
 
-
+	// Initialize data file where the timing results will be stored
 	FILE *f = fopen("data.csv", "w");
 
-for ( BLOCKSIZE = 1; BLOCKSIZE < 1024; BLOCKSIZE++ )
-{
-	int nBlocks = ARRAYSIZE/BLOCKSIZE + (ARRAYSIZE%BLOCKSIZE == 0?0:1);
-	printf("Nblocks: %i", nBlocks);
+	for (BLOCKSIZE = 1; BLOCKSIZE < 1024; BLOCKSIZE++)
+	{
+		// Calculate amount of blocks needed
+		int nBlocks = ARRAYSIZE / BLOCKSIZE + (ARRAYSIZE % BLOCKSIZE == 0 ? 0 : 1);
+		printf("Nblocks: %i", nBlocks);
 
-	// Measure time
-    StopWatchInterface *timer = 0;
-    sdkCreateTimer(&timer);
-    sdkStartTimer(&timer);
+		// Start timer
+		StopWatchInterface *timer = 0;
+		sdkCreateTimer(&timer);
+		sdkStartTimer(&timer);
 
-	add <<< nBlocks, BLOCKSIZE >>> (a_dev, b_dev, out_dev);
+		////////////////////////////////////////////////////////////////////////////////
+		// GPU -- comment / uncomment to run 'ADD' / 'INVERT'
+		////////////////////////////////////////////////////////////////////////////////
+		add<<<nBlocks, BLOCKSIZE>>>(a_dev, b_dev, out_dev);
+		// invert <<< nBlocks, BLOCKSIZE >>> ( a_dev, out_dev );
 
-    sdkStopTimer(&timer);
-    printf("Processing time: %f (ms)\n", sdkGetTimerValue(&timer));
+		////////////////////////////////////////////////////////////////////////////////
+		// CPU -- comment / uncomment to run 'ADD' / 'INVERT'
+		////////////////////////////////////////////////////////////////////////////////
+		// add( a_dev, b_dev, out_dev );
+		// invert ( a_dev, out_dev );
 
+		// Stop timer
+		sdkStopTimer(&timer);
+		printf("Processing time: %f (ms)\n", sdkGetTimerValue(&timer));
 
-    fprintf(f, "%d,%f\n", BLOCKSIZE, sdkGetTimerValue(&timer));
+		// Write timing results to file
+		fprintf(f, "%d,%f\n", BLOCKSIZE, sdkGetTimerValue(&timer));
 
+		// Verwijder timer
+		sdkDeleteTimer(&timer);
 
+	} //End for
 
-    sdkDeleteTimer(&timer);
-
-}//End for
-
+	// Close the file
 	fclose(f);
 
-
-
 	//Step 4: Retrieve result
-	cudaMemcpy(a_host, a_dev, ARRAYSIZE * sizeof(int), 	cudaMemcpyDeviceToHost);
-	cudaMemcpy(b_host, b_dev, ARRAYSIZE * sizeof(int), 	cudaMemcpyDeviceToHost);
-	cudaMemcpy(out_host, out_dev, ARRAYSIZE * sizeof(int), 	cudaMemcpyDeviceToHost);
+	cudaMemcpy(a_host, a_dev, ARRAYSIZE * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(b_host, b_dev, ARRAYSIZE * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(out_host, out_dev, ARRAYSIZE * sizeof(int), cudaMemcpyDeviceToHost);
 
+	// Free up the used memory
+	free(a_host);
+	free(b_host);
+	free(out_host);
+	cudaFree(a_dev);
+	cudaFree(b_dev);
+	cudaFree(out_dev);
 
-	//end of  program
-	//cleanup: VERY IMPORTANT!!!
-	free(a_host); free(b_host); free(out_host); cudaFree(a_dev); cudaFree(b_dev); cudaFree(out_dev);
-
-return 0;
+	return 0;
 }
